9090320de8f100d8e653db5c46dac7f5e8d168c9
soju-2 <68412506+soju-2@users.noreply.github.com>
2020-07-16 22:59:01 -0700

    Fix animation and add background

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /**
-1. fix animation for hover
-2. hover affect with menu and order images
-3. Save code in github.
-4. add rectangle for seperation
+1. fix animation for hover - checked
+2. hover affect with menu and order images - checked
+3. Save code in github - checked
+4. add rectangle for seperation - checked
+
+TODO:
+- Fix height for logo image.
+- add transition effect
 **/
